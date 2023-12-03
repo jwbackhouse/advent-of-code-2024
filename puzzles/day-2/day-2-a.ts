36adfa3ba50c3923db3867b6effc2075dbb787ec
James Backhouse <james.backhouse@edume.com>
2023-12-03 21:27:43 +0000

    Day 2 solution.

--- puzzles/day-2/day-2-a.ts
+++ puzzles/day-2/day-2-a.ts
@@ -1,9 +1,51 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+const limits = {
+  red: 12,
+  green: 13,
+  blue: 14,
+};
+
 export async function day2a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  let result = 0;
+
+  data.forEach((row) => {
+    const [gameId, sets] = row.split(': ');
+    const [_, id] = gameId.split(' ');
+    const turns = sets.split('; ');
+
+    let isValidRow = true;
+
+    turns.forEach((turn) => {
+      let cubeMap = {
+        red: 0,
+        green: 0,
+        blue: 0,
+      };
+      const cubes = turn.split(', ');
+
+      cubes.forEach((cube) => {
+        const [number, colour] = cube.split(' ');
+        cubeMap[colour] += parseInt(number);
+      });
+
+      Object.entries(cubeMap).forEach(([colour, number]) => {
+        if (number > limits[colour]) {
+          isValidRow = false;
+        }
+      });
+    });
+
+    if (isValidRow) {
+      result += parseInt(id);
+    }
+    // console.log('res', id, isValidRow, result);
+  });
+
+  return result;
 }
 
 const answer = await day2a();
--- puzzles/day-2/day-2-b.ts
+++ puzzles/day-2/day-2-b.ts
@@ -1,9 +1,45 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+const limits = {
+  red: 12,
+  green: 13,
+  blue: 14,
+};
+
 export async function day2b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  let result = 0;
+
+  data.forEach((row) => {
+    const [gameId, sets] = row.split(': ');
+    const [_, id] = gameId.split(' ');
+    const turns = sets.split('; ');
+
+    let cubeMap: Record<string, number> = {
+      red: 0,
+      green: 0,
+      blue: 0,
+    };
+    turns.forEach((turn) => {
+      const cubes = turn.split(', ');
+
+      cubes.forEach((cube) => {
+        const [number, colour] = cube.split(' ');
+        if (cubeMap[colour] < parseInt(number)) {
+          cubeMap[colour] = parseInt(number);
+        }
+      });
+    });
+    console.log(cubeMap);
+
+    const power = Object.values(cubeMap).reduce((acc, curr) => acc * curr, 1);
+    console.log('power: ', power);
+    result += power;
+  });
+
+  return result;
 }
 
 const answer = await day2b();
