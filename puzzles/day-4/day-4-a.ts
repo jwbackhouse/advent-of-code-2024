f3e13801b4bdfd9e035ea52b72b9a5fc702cadec
James Backhouse <james.backhouse@edume.com>
2023-12-05 21:01:49 +0000

    Day 4 solution.

--- puzzles/day-4/day-4-a.ts
+++ puzzles/day-4/day-4-a.ts
@@ -3,7 +3,31 @@ import chalk from 'chalk';
 
 export async function day4a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  let result = 0;
+
+  data.forEach((row) => {
+    const [id, gameData] = row.split(': ');
+    const [winningNumStr, guessStr] = gameData.split(' | ');
+
+    const winningNums = winningNumStr.split(' ').filter(Boolean);
+    const guesses = guessStr.split(' ').filter(Boolean);
+
+    const winMap = {};
+    let correctGuesses = 0;
+    winningNums.forEach((num) => {
+      winMap[num] = 1;
+    });
+
+    guesses.forEach((guess) => {
+      correctGuesses += winMap[guess] ?? 0;
+    });
+
+    const score = correctGuesses ? 1 * 2 ** (correctGuesses - 1) : 0;
+    result += score;
+  });
+
+  return result;
 }
 
 const answer = await day4a();
--- puzzles/day-4/day-4-b.ts
+++ puzzles/day-4/day-4-b.ts
@@ -3,7 +3,40 @@ import chalk from 'chalk';
 
 export async function day4b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  let tally: Record<string, number> = {};
+
+  data.forEach((row, index) => {
+    tally[index] = tally[index] ? ++tally[index] : 1;
+
+    const [id, gameData] = row.split(': ');
+    const [winningNumStr, guessStr] = gameData.split(' | ');
+
+    const winningNums = winningNumStr.split(' ').filter(Boolean);
+    const guesses = guessStr.split(' ').filter(Boolean);
+
+    const winMap = {};
+    let correctGuesses = 0;
+    winningNums.forEach((num) => {
+      winMap[num] = 1;
+    });
+
+    guesses.forEach((guess) => {
+      correctGuesses += winMap[guess] ?? 0;
+    });
+
+    const nextIndex = index + 1;
+    for (let i = nextIndex; i < correctGuesses + nextIndex; i++) {
+      const additionalTally = tally[index] || 0;
+      const existingTally = tally[i];
+      tally[i] = existingTally
+        ? existingTally + additionalTally
+        : additionalTally;
+    }
+  });
+  const result = Object.values(tally).reduce((acc, curr) => acc + curr, 0);
+
+  return result;
 }
 
 const answer = await day4b();
