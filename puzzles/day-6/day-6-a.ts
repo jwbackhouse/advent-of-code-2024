b2a65d47aa451f723cfe1a7d35f58cda7dbe8b39
James Backhouse <james.backhouse@edume.com>
2023-12-07 07:39:58 +0000

    Day 6 solution.

--- puzzles/day-6/day-6-a.ts
+++ puzzles/day-6/day-6-a.ts
@@ -1,9 +1,50 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+const getDistance = (time: number, speed: number) => time * speed;
+
+const processRace = (totalTime: number, record: number) => {
+  let newRecords = 0;
+  let maxDistance = 0;
+  for (let i = 1; i < totalTime; i++) {
+    const remainingTime = totalTime - i;
+    const distance = getDistance(remainingTime, i);
+
+    if (distance > record) {
+      newRecords++;
+    }
+    // small optimization
+    if (distance > maxDistance) {
+      maxDistance = distance;
+    }
+    if (distance < maxDistance && distance < record) {
+      break;
+    }
+  }
+
+  return newRecords;
+};
+
 export async function day6a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  const parseRow = (row: string) =>
+    row
+      .split(' ')
+      .filter((char) => !isNaN(Number(char)) && char !== '')
+      .map(Number);
+
+  const time = parseRow(data[0]);
+  const record = parseRow(data[1]);
+
+  let result = 1;
+
+  time.forEach((t, i) => {
+    const newRecords = processRace(t, record[i]);
+    result *= newRecords;
+  });
+
+  return result;
 }
 
 const answer = await day6a();
--- puzzles/day-6/day-6-b.ts
+++ puzzles/day-6/day-6-b.ts
@@ -1,9 +1,52 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+const getDistance = (time: number, speed: number) => time * speed;
+
+const processRace = (totalTime: number, record: number) => {
+  let newRecords = 0;
+  let maxDistance = 0;
+  for (let i = 1; i < totalTime; i++) {
+    const remainingTime = totalTime - i;
+    const distance = getDistance(remainingTime, i);
+
+    if (distance > record) {
+      newRecords++;
+    }
+    // small optimization
+    if (distance > maxDistance) {
+      maxDistance = distance;
+    }
+    if (distance < maxDistance && distance < record) {
+      break;
+    }
+  }
+
+  return newRecords;
+};
+
 export async function day6b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  const parseRow = (row: string) =>
+    Number(
+      row
+        .split(' ')
+        .filter((char) => !isNaN(Number(char)) && char !== '')
+        .join('')
+    );
+
+  const time = parseRow(data[0]);
+  const record = parseRow(data[1]);
+
+  let result = processRace(time, record);
+
+  // time.forEach((t, i) => {
+  //   const newRecords = processRace(t, record[i]);
+  //   result *= newRecords;
+  // });
+
+  return result;
 }
 
 const answer = await day6b();
