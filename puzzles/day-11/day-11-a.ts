50b5d7e1a51e9ff5e57d1797063647591470f321
James Backhouse <james.backhouse@edume.com>
2023-12-11 20:56:09 +0000

    Day 11 solution.

--- puzzles/day-11/day-11-a.ts
+++ puzzles/day-11/day-11-a.ts
@@ -1,9 +1,97 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+type Coordinate = [number, number];
+type StarMap = Array<Array<string>>;
+
+const getIsEmpty = (char: string) => char === '.';
+
+const getExpandedMap = (data: Array<string>) => {
+  const expandedMap: StarMap = [];
+  const columns = [];
+
+  data.forEach((row) => {
+    const rowArray = row.split('');
+    expandedMap.push(rowArray);
+
+    const isEmpty = rowArray.every(getIsEmpty);
+    if (isEmpty) {
+      expandedMap.push(rowArray);
+    }
+
+    rowArray.forEach((char, colIndex) => {
+      if (!columns[colIndex]) {
+        columns[colIndex] = [];
+      }
+      columns[colIndex].push(char);
+    });
+  });
+
+  const emptyColumns = columns.map((column) => {
+    const isEmpty = column.every(getIsEmpty);
+    return isEmpty;
+  });
+
+  let isEmptyCount = 0;
+  emptyColumns.forEach((isEmpty, index) => {
+    if (isEmpty) {
+      expandedMap.forEach((row) => {
+        row.splice(index + isEmptyCount, 0, '.');
+      });
+      isEmptyCount++;
+    }
+  });
+
+  return expandedMap;
+};
+
+const getCoordinates = (map: StarMap) => {
+  const coordinates = [];
+
+  map.forEach((row, rowIndex) => {
+    row.forEach((char, colIndex) => {
+      if (char === '#') {
+        coordinates.push([rowIndex, colIndex]);
+      }
+    });
+  });
+
+  return coordinates;
+};
+
+const getPathLength = (
+  [startRow, startCol]: Coordinate,
+  [endRow, endCol]: Coordinate
+) => {
+  const rowDiff = Math.abs(startRow - endRow);
+  const colDiff = Math.abs(startCol - endCol);
+
+  return rowDiff + colDiff;
+};
+
+const getTotalPathLenth = (coordinates: Array<Coordinate>) => {
+  let totalPathLength = 0;
+
+  coordinates.forEach((coordinate, index) => {
+    let nextIndex = index + 1;
+    while (coordinates[nextIndex]) {
+      const nextCoordinate = coordinates[nextIndex];
+      totalPathLength += getPathLength(coordinate, nextCoordinate);
+      nextIndex++;
+    }
+  });
+
+  return totalPathLength;
+};
+
 export async function day11a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  const expandedMap = getExpandedMap(data);
+  const coordinates = getCoordinates(expandedMap);
+
+  const totalPathLength = getTotalPathLenth(coordinates);
+  return totalPathLength;
 }
 
 const answer = await day11a();
--- puzzles/day-11/day-11-b.ts
+++ puzzles/day-11/day-11-b.ts
@@ -1,9 +1,139 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+type Coordinate = [number, number];
+type StarMap = Array<Array<string>>;
+
+const EXPANSION = 999999;
+
+const getIsEmpty = (char: string) => char === '.';
+
+const addIfEmpty = (
+  array: Array<string>,
+  index: number,
+  target: Array<number>
+) => {
+  const isEmpty = array.every(getIsEmpty);
+  if (isEmpty) {
+    target.push(index);
+  }
+};
+
+const getEmptyLines = (map: StarMap) => {
+  const emptyRows: Array<number> = [];
+  const emptyColumns: Array<number> = [];
+
+  const columns: Array<Array<string>> = [];
+
+  map.forEach((row, rowIndex) => {
+    addIfEmpty(row, rowIndex, emptyRows);
+
+    row.forEach((char, colIndex) => {
+      if (!columns[colIndex]) {
+        columns[colIndex] = [];
+      }
+      columns[colIndex].push(char);
+    });
+  });
+
+  columns.forEach((column, colIndex) => {
+    addIfEmpty(column, colIndex, emptyColumns);
+  });
+
+  return { emptyRows, emptyColumns };
+};
+
+const getCoordinates = (map: StarMap) => {
+  const coordinates = [];
+
+  map.forEach((row, rowIndex) => {
+    row.forEach((char, colIndex) => {
+      if (char === '#') {
+        coordinates.push([rowIndex, colIndex]);
+      }
+    });
+  });
+
+  return coordinates;
+};
+
+const addExpansion = (
+  emptyIndices: Array<number>,
+  start: number,
+  end: number
+) => {
+  let total = 0;
+
+  emptyIndices.forEach((emptyIndex) => {
+    if (
+      (start < emptyIndex && emptyIndex < end) ||
+      (end < emptyIndex && emptyIndex < start)
+    ) {
+      total = total + EXPANSION;
+    }
+  });
+
+  return total;
+};
+
+const getPathLength = ({
+  coordinate: [startRow, startCol],
+  nextCoordinate: [endRow, endCol],
+  emptyColumns,
+  emptyRows,
+}: {
+  coordinate: Coordinate;
+  nextCoordinate: Coordinate;
+  emptyColumns: Array<number>;
+  emptyRows: Array<number>;
+}) => {
+  let rowDiff = Math.abs(startRow - endRow);
+  let colDiff = Math.abs(startCol - endCol);
+
+  colDiff += addExpansion(emptyColumns, startCol, endCol);
+  rowDiff += addExpansion(emptyRows, startRow, endRow);
+
+  return rowDiff + colDiff;
+};
+
+const getTotalPathLength = (
+  coordinates: Array<Coordinate>,
+  emptyColumns: Array<number>,
+  emptyRows: Array<number>
+) => {
+  let totalPathLength = 0;
+
+  coordinates.forEach((coordinate, index) => {
+    let nextIndex = index + 1;
+    while (coordinates[nextIndex]) {
+      const nextCoordinate = coordinates[nextIndex];
+      totalPathLength += getPathLength({
+        coordinate,
+        nextCoordinate,
+        emptyColumns,
+        emptyRows,
+      });
+      nextIndex++;
+    }
+  });
+
+  return totalPathLength;
+};
+
 export async function day11b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  const map = data.map((row) => row.split(''));
+  const { emptyColumns, emptyRows } = getEmptyLines(map);
+
+  const coordinates = getCoordinates(map);
+
+  const totalPathLength = getTotalPathLength(
+    coordinates,
+    emptyColumns,
+    emptyRows
+  );
+  return totalPathLength;
 }
 
 const answer = await day11b();
