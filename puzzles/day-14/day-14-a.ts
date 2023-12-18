02b703c2d1522d72eb1521a186e7da0a0cb25494
James Backhouse <james.backhouse@edume.com>
2023-12-18 20:47:44 +0000

    Day 14a solution.

--- puzzles/day-14/day-14-a.ts
+++ puzzles/day-14/day-14-a.ts
@@ -1,9 +1,71 @@
-import { readData } from '../../shared.ts';
+import { readFile } from 'fs/promises';
 import chalk from 'chalk';
 
+const convertColumnsToRows = (data: Array<string>) => {
+  const columns: Array<Array<string>> = [];
+  data.forEach((col: string) => {
+    const colArray = col.split('');
+    colArray.forEach((value: string, index: number) => {
+      if (!columns[index]) {
+        columns[index] = [];
+      }
+      columns[index].push(value);
+    });
+  });
+
+  return columns;
+};
+
+const tiltColumns = (columns: Array<Array<string>>) => {
+  let newColumns = [];
+  columns.forEach((column: Array<string>) => {
+    const components = column.join('').split('#');
+    let newCol = [];
+
+    components.forEach((component: string) => {
+      const componentArray = component.split('');
+      const sorted = componentArray.sort((a, b) => {
+        if (a === '.' && b === 'O') {
+          return 1;
+        }
+        if (a === 'O' && b === '.') {
+          return -1;
+        }
+        return 0;
+      });
+      newCol.push(sorted.join(''));
+    });
+    newColumns.push(newCol.join('#'));
+  });
+
+  return newColumns;
+};
+
+const checkWeight = (columns: Array<string>) => {
+  const length = columns.length;
+
+  const weight = columns.reduce((acc: number, column: string) => {
+    let weight = 0;
+    column.split('').forEach((item: string, index: number) => {
+      if (item === 'O') {
+        weight += length - index;
+      }
+    });
+
+    return acc + weight;
+  }, 0);
+
+  return weight;
+};
+
 export async function day14a(dataPath?: string) {
-  const data = await readData(dataPath);
-  return 0;
+  const fileName = dataPath || process.argv[2];
+  const data = (await readFile(fileName)).toString();
+  const rows = data.split('\n');
+  const columns = convertColumnsToRows(rows);
+
+  const tilted = tiltColumns(columns);
+  return checkWeight(tilted);
 }
 
 const answer = await day14a();
